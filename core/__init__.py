03a77893cb2df00f87c193bfdd7397e62291e255
rgazizov232@gmai.com <rgazizov232@gmai.com>
2025-07-09 11:10:23 +0500

    Дашборд: вынесены все основные файлы дашборда в отдельную папку для удобства работы

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+"""
+Core components for the trading dashboard
+"""
+
+from .dashboard_core import DashboardCore
+from .dashboard_ui import DashboardUI
+from .dashboard_logic import DashboardLogic
+
+__all__ = ['DashboardCore', 'DashboardUI', 'DashboardLogic'] 
